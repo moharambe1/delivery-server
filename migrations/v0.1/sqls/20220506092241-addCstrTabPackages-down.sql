5d7d3c7f84becbb6e6924560b2813d9dd03768bc
mohammedi ahmed dhia eddine <moharambe18@gmail.com>
2022-05-06 18:54:59 +0100

    fix db-migrate package. add Cstrainet to tab packages on idClient,idDelivery,idCreater column.

--- migrations/v0.1/sqls/20220506092241-addCstrTabPackages-down.sql
+++ migrations/v0.1/sqls/20220506092241-addCstrTabPackages-down.sql
@@ -1,1 +1,10 @@
 /* Replace with your SQL commands */
+
+ALTER TABLE PACKAGES
+DROP CONSTRAINT constraint_idClient;
+
+ALTER TABLE PACKAGES
+DROP CONSTRAINT constraint_idDelivery;
+
+ALTER TABLE PACKAGES
+DROP CONSTRAINT constraint_idCreater;
