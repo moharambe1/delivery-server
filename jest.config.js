3c7aa0ad6b7268d5517ac7ee18ace59fdae1fe34
mohammedi ahmed dhia eddine <moharambe18@gmail.com>
2022-05-14 12:25:57 +0100

    disable trailingComma in .prettierrc  update jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,20 @@
-export default {
-  testEnvironment: 'node',
-  preset: 'ts-jest/presets/default-esm',
-  globals: {
-    'ts-jest': {
-      useESM: true,
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+
+module.exports = async () => {
+  return {
+    testEnvironment: 'node',
+    preset: 'ts-jest/presets/default-esm',
+    globals: {
+      'ts-jest': {
+        useESM: true
+      }
     },
-  },
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.d.ts',
-  ],
+    moduleNameMapper: {
+      '^(\\.{1,2}/.*)\\.js$': '$1'
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts']
+  };
 };
